dbed4d67a58d4a03b2696418a48a0e8af4064c60
Percy Kuang <percykuang@foxmail.com>
2026-04-14 12:03:49 +0800

    chore(repo): add shared scope

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -14,7 +14,16 @@ const allowedTypes = [
   'test',
 ]
 
-const allowedScopes = ['web', 'admin', 'tokens', 'icons', 'ui', 'repo', 'db']
+const allowedScopes = [
+  'web',
+  'admin',
+  'tokens',
+  'icons',
+  'ui',
+  'repo',
+  'db',
+  'shared',
+]
 
 // commitlint 的规则数组格式通常是：
 // [严重级别, 条件, 规则值]
